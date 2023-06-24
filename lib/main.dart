48ac1a45f2b06aaa515901a53d075d5f3c4e1cb8
amirtorabi <dfd.torabi@gmail.com>
2023-06-24 16:41:35 +0330

    added dotted border on story profiles

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:blog_club/data.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:gradient_borders/box_borders/gradient_box_border.dart';
 
@@ -13,38 +14,32 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final Color primaryTextColor = Color(0xFF2D4379);
-    final Color secondaryTextColor = Color(0xFF0D253C);
+    const primaryTextColor = Color(0xff0D253C);
+    const secondaryTextColor = Color(0xff2D4379);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // TRY THIS: Try running your application with "flutter run". You'll see
-          // the application has a blue toolbar. Then, without quitting the app,
-          // try changing the seedColor in the colorScheme below to Colors.green
-          // and then invoke "hot reload" (save your changes or press the "hot
-          // reload" button in a Flutter-supported IDE, or press "r" if you used
-          // the command line to start the app).
-          //
-          // Notice that the counter didn't reset back to zero; the application
-          // state is not lost during the reload. To reset the state, use hot
-          // restart instead.
-          //
-          // This works for code too, not just values: Most code changes can be
-          // tested with just a hot reload.
-          textTheme: TextTheme(
-              titleSmall: TextStyle(
-                  fontFamily: defaultFontFamily,
-                  color: primaryTextColor,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500),
-              titleLarge: TextStyle(
-                  fontFamily: defaultFontFamily,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
-                  color: secondaryTextColor)),
-          primarySwatch: Colors.blue),
+        primarySwatch: Colors.blue,
+        textTheme: const TextTheme(
+            titleMedium: TextStyle(
+                fontFamily: defaultFontFamily,
+                color: secondaryTextColor,
+                fontWeight: FontWeight.w200,
+                fontSize: 18),
+            titleLarge: TextStyle(
+                fontFamily: defaultFontFamily,
+                fontWeight: FontWeight.bold,
+                color: primaryTextColor),
+            headlineMedium: TextStyle(
+                fontFamily: defaultFontFamily,
+                fontWeight: FontWeight.w700,
+                fontSize: 24,
+                color: primaryTextColor),
+            bodyMedium: TextStyle(
+                fontFamily: defaultFontFamily,
+                color: secondaryTextColor,
+                fontSize: 12)),
+      ),
       home: const HomeScreen(),
     );
   }
@@ -69,89 +64,149 @@ class HomeScreen extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('Hi, Amir!', style: myTheme.textTheme.titleSmall),
+                  Text('Hi, Amir!', style: myTheme.textTheme.titleMedium),
                   Image.asset(
                     'assets/img/icons/notification.png',
-                    width: 24,
-                    height: 24,
+                    width: 32,
+                    height: 32,
                   ),
                 ],
               ),
             ),
             Padding(
-              padding: const EdgeInsets.fromLTRB(32, 0, 32, 24),
+              padding: const EdgeInsets.fromLTRB(32, 0, 0, 16),
               child: Text(
                 'Explore today’s',
-                style: myTheme.textTheme.titleLarge,
+                style: myTheme.textTheme.headlineMedium,
               ),
             ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: 92,
-              child: ListView.builder(
-                itemCount: stories.length,
-                scrollDirection: Axis.horizontal,
-                padding: const EdgeInsets.fromLTRB(
-                    32, 0, 32, 0), //padding az chap & rast asli
-                itemBuilder: (context, index) {
-                  final story = stories[index];
-
-                  //sakhte har kodom az ajzaye list
-                  return Container(
-                    width: 68,
-                    height: 92,
-                    margin: const EdgeInsets.fromLTRB(4, 0, 4, 0),
-                    child: Column(
-                      children: [
-                        Stack(
-                          children: [
-                            Container(
-                              width: 68,
-                              height: 68,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
-                                  border: const GradientBoxBorder(
-                                      gradient: LinearGradient(
-                                          begin: Alignment.topLeft,
-                                          end: Alignment.bottomRight,
-                                          colors: [
-                                            Color(0xff376AED),
-                                            Color(0xff49B0E2),
-                                            Color(0xff9CECFB),
-                                          ]),
-                                      width: 2)),
-                              padding: const EdgeInsets.all(4),
-                              child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(16),
-                                  child: Image.asset(
-                                    'assets/img/stories/${story.imageFileName}',
-                                    width: 54,
-                                    height: 54,
-                                  )),
-                            ),
-                            Positioned(
-                              //dakhele stack az samte rast cheghad fasele dashte bashe
-                              right: 0,
-                              bottom: 0,
-                              child: Image.asset(
-                                'assets/img/icons/${story.iconFileName}',
-                                width: 24,
-                                height: 24,
-                              ),
-                            )
-                          ],
-                        ),
-                        const SizedBox(height: 4),
-                        Text(story.name),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            )
+            _StoryList(stories: stories, myTheme: myTheme)
           ],
         )),
       ),
     );
+  }
+}
+
+class _StoryList extends StatelessWidget {
+  const _StoryList({
+    super.key,
+    required this.stories,
+    required this.myTheme,
+  });
+
+  final List<StoryData> stories;
+  final ThemeData myTheme;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: 92,
+      child: ListView.builder(
+        itemCount: stories.length,
+        physics: const BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.fromLTRB(
+            32, 0, 32, 0), //padding az chap & rast asli
+        itemBuilder: (context, index) {
+          final story = stories[index];
+
+          //sakhte har kodom az ajzaye list
+          return _Story(story: story, myTheme: myTheme);
+        },
+      ),
+    );
+  }
+}
+
+class _Story extends StatelessWidget {○
+  const _Story({
+    super.key,
+    required this.story,
+    required this.myTheme,
+  });
+
+  final StoryData story;
+  final ThemeData myTheme;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 68,
+      height: 100,
+      margin: const EdgeInsets.fromLTRB(4, 2, 4, 0),
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              story.isViewed ? _viewedProfileImage() : _normalProfileImage(),
+              Positioned(
+                //dakhele stack az samte rast cheghad fasele dashte bashe
+                right: 0,
+                bottom: 0,
+                child: Image.asset(
+                  'assets/img/icons/${story.iconFileName}',
+                  width: 24,
+                  height: 24,
+                ),
+              )
+            ],
+          ),
+          const SizedBox(height: 8),
+          Text(
+            story.name,
+            style: myTheme.textTheme.bodyMedium,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _normalProfileImage() {
+    return Container(
+      width: 68,
+      height: 68,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          border: const GradientBoxBorder(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xff376AED),
+                    Color(0xff49B0E2),
+                    Color(0xff9CECFB),
+                  ]),
+              width: 2)),
+      padding: const EdgeInsets.all(4),
+      child: _profileImage(),
+    );
+  }
+
+  Widget _viewedProfileImage() {
+    return SizedBox(
+      width: 68,
+      height: 68,
+      child: DottedBorder(
+        borderType: BorderType.RRect,
+        strokeWidth: 2,
+        dashPattern: const [8, 3],
+        radius: const Radius.circular(20),
+        color: const Color(0xff7B8BB2),
+        padding: const EdgeInsets.all(7),
+        child: _profileImage(),
+      ),
+    );
+  }
+
+  Widget _profileImage() {
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: Image.asset(
+          'assets/img/stories/${story.imageFileName}',
+          width: 54,
+          height: 54,
+        ));
   }
 }
